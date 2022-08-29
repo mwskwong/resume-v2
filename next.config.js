ec2e874cffbcd7786a6ca57b994fb3f5eda8aee0
Matthew Kwong <matthewkwong2@gmail.com>
2022-08-29 13:29:21 +0800

    feat: Analyze bundle when build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE_BUNDLE === "true"
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -7,4 +12,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
