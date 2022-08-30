6bdcba265c958e9232ada1d70369fbe71b2cf6be
Matthew Kwong <matthewkwong2@gmail.com>
2022-08-30 13:12:43 +0800

    fix: header

--- next.config.js
+++ next.config.js
@@ -51,17 +51,17 @@ const nextConfig = {
     return config;
   },
   headers: async () => [
-    {
-      source: "/:path*",
-      headers: [
-        // { key: "X-DNS-Prefetch-Control", value: "on" },
-        // { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
-        // { key: "X-XSS-Protection", value: "1; mode=block" },
-        // { key: "X-Frame-Options", value: "SAMEORIGIN" },
-        // { key: "X-Content-Type-Options", value: "nosniff" },
-        // { key: "Referrer-Policy", value: "no-referrer-when-downgrade" }
-      ]
-    }
+    // {
+    //   source: "/:path*",
+    //   headers: [
+    //     // { key: "X-DNS-Prefetch-Control", value: "on" },
+    //     // { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+    //     // { key: "X-XSS-Protection", value: "1; mode=block" },
+    //     // { key: "X-Frame-Options", value: "SAMEORIGIN" },
+    //     // { key: "X-Content-Type-Options", value: "nosniff" },
+    //     // { key: "Referrer-Policy", value: "no-referrer-when-downgrade" }
+    //   ]
+    // }
   ],
   experimental: {
     runtime: "experimental-edge",
