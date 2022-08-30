74d0fdea208f4ec46a0aef0dc5e26eb4f498349c
Matthew Kwong <matthewkwong2@gmail.com>
2022-08-30 12:58:53 +0800

    Revert "feat: Disable `Referrer-Policy`"
    This reverts commit a5af353031316e0353948b41d345ee2dc81afb39.

--- next.config.js
+++ next.config.js
@@ -58,7 +58,8 @@ const nextConfig = {
         { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
         { key: "X-XSS-Protection", value: "1; mode=block" },
         { key: "X-Frame-Options", value: "SAMEORIGIN" },
-        { key: "X-Content-Type-Options", value: "nosniff" }
+        { key: "X-Content-Type-Options", value: "nosniff" },
+        { key: "Referrer-Policy", value: "no-referrer-when-downgrade" }
       ]
     }
   ],
