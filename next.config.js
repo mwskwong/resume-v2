76029848c8abff55fa56c4edf963d0ce59269afa
Matthew Kwong <matthewkwong2@gmail.com>
2022-08-30 13:16:03 +0800

    Revert "feat: disable all headers"
    This reverts commit 7952e97879303e045a6c24fc98378727e12be78f.

--- next.config.js
+++ next.config.js
@@ -54,12 +54,12 @@ const nextConfig = {
     {
       source: "/:path*",
       headers: [
-        // { key: "X-DNS-Prefetch-Control", value: "on" },
-        // { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
-        // { key: "X-XSS-Protection", value: "1; mode=block" },
-        // { key: "X-Frame-Options", value: "SAMEORIGIN" },
-        // { key: "X-Content-Type-Options", value: "nosniff" },
-        // { key: "Referrer-Policy", value: "no-referrer-when-downgrade" }
+        { key: "X-DNS-Prefetch-Control", value: "on" },
+        { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+        { key: "X-XSS-Protection", value: "1; mode=block" },
+        { key: "X-Frame-Options", value: "SAMEORIGIN" },
+        { key: "X-Content-Type-Options", value: "nosniff" },
+        { key: "Referrer-Policy", value: "no-referrer-when-downgrade" }
       ]
     }
   ],
