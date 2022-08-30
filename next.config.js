a5af353031316e0353948b41d345ee2dc81afb39
Matthew Kwong <matthewkwong2@gmail.com>
2022-08-30 12:55:01 +0800

    feat: Disable `Referrer-Policy`

--- next.config.js
+++ next.config.js
@@ -58,8 +58,7 @@ const nextConfig = {
         { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
         { key: "X-XSS-Protection", value: "1; mode=block" },
         { key: "X-Frame-Options", value: "SAMEORIGIN" },
-        { key: "X-Content-Type-Options", value: "nosniff" },
-        { key: "Referrer-Policy", value: "no-referrer-when-downgrade" }
+        { key: "X-Content-Type-Options", value: "nosniff" }
       ]
     }
   ],
