085123d079f4b7368c76887e1aad39ed111b5edf
Matthew Kwong <matthewkwong2@gmail.com>
2022-08-30 19:04:43 +0800

    feat: Improve form UX and DX

--- components/index/Contact/useContactSx.ts
+++ components/index/Contact/useContactSx.ts
@@ -2,6 +2,13 @@ import { UseSx } from "types";
 
 const useSx: UseSx = sx => ({
   root: sx,
+  errorMessagesContainer: {
+    mt: 1
+  },
+  errorMessage: {
+    typography: "body2",
+    color: "error.main"
+  },
   submitButton: {
     float: "right",
     mt: 4
