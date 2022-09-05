d87f819bbd03a2174a8a761ac66f203d460202d5
Matthew Kwong <matthewkwong2@gmail.com>
2022-09-06 00:08:26 +0800

    test: Test Vercel deployment

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "bzfyrk",
   e2e: {
-    baseUrl: "http://localhost:3000",
     viewportWidth: 1920,
     viewportHeight: 1080
   }
