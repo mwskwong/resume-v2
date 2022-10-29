30300de5c3e26bc4625b4297456025c1748ccd04
Matthew Kwong <matthewkwong2@gmail.com>
2022-10-29 13:14:32 +0800

    feat: Enable Vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,19 @@ import "utils/wdyr";
 
 import { CssBaseline, Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material";
 
+import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import type { FC } from "react";
 import brandingTheme from "brandingTheme";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <CssVarsProvider theme={brandingTheme} disableTransitionOnChange>
-    <CssBaseline />
-    <Component {...pageProps} />
-  </CssVarsProvider>
+  <>
+    <CssVarsProvider theme={brandingTheme} disableTransitionOnChange>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CssVarsProvider>
+    <Analytics />
+  </>
 );
 
 export default App;
