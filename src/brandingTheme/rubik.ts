55396ef318ad6191a2101f294fa9eb7a1fb7bb86
Matthew Kwong <matthew.kwong@tecpal.com>
2022-11-08 09:26:03 +0800

    fix: Assign rubik to HTML className

---
+++ src/brandingTheme/rubik.ts
@@ -0,0 +1,5 @@
+import { Rubik } from "@next/font/google";
+
+const rubik = Rubik({ subsets: ["latin"], display: "swap" });
+
+export default rubik;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,9 @@
+import { rubik } from "brandingTheme";
 import { Head, Html, Main, NextScript } from "next/document";
 import { FC } from "react";
 
 const Document: FC = () => (
-  <Html lang="en">
+  <Html lang="en" className={rubik.className}>
     <Head />
     <body>
       <Main />
