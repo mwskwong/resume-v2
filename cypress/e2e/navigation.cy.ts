af8c518ce1d8f65313edb091b674c056a5aec8c9
Matthew Kwong <matthew.kwong@tecpal.com>
2022-12-09 09:45:35 +0800

    test: Add a small delay after scrolling

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -52,6 +52,7 @@ describe("Navigation", () => {
         .window()
         .its("scrollY")
         .should("not.equal", 0);
+      cy.wait(50);
       cy.get("[data-cy='logo']").click();
       cy.location("hash").should("equal", `#${nav.HOME.id}`);
       cy.window().its("scrollY").should("equal", 0);
@@ -65,6 +66,7 @@ describe("Navigation", () => {
         .window()
         .its("scrollY")
         .should("not.equal", 0);
+      cy.wait(50);
       cy.get("[data-cy='scrollToTop']").click();
       cy.location("hash").should("equal", `#${nav.HOME.id}`);
       cy.window().its("scrollY").should("equal", 0);
