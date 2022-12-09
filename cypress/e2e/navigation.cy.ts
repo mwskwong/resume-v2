eeef829a5d5ecd4844ae668cebcdeefdb86134d5
Matthew Kwong <matthewkwong2@gmail.com>
2022-12-09 19:46:40 +0800

    test: double the delay after scroll to bottom

--- cypress/e2e/navigation.cy.ts
+++ cypress/e2e/navigation.cy.ts
@@ -52,7 +52,7 @@ describe("Navigation", () => {
         .window()
         .its("scrollY")
         .should("not.equal", 0);
-      cy.wait(50);
+      cy.wait(100);
       cy.get("[data-cy='logo']").click();
       cy.location("hash").should("equal", `#${nav.HOME.id}`);
       cy.window().its("scrollY").should("equal", 0);
@@ -66,7 +66,7 @@ describe("Navigation", () => {
         .window()
         .its("scrollY")
         .should("not.equal", 0);
-      cy.wait(50);
+      cy.wait(100);
       cy.get("[data-cy='scrollToTop']").click();
       cy.location("hash").should("equal", `#${nav.HOME.id}`);
       cy.window().its("scrollY").should("equal", 0);
