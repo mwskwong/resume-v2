ae39121f45215bbe4f23b29f26c58d1838b1f296
Matthew Kwong <matthewkwong2@gmail.com>
2023-01-28 23:02:06 +0800

    refactor: Use PascalCase for components in `/app`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import Footer from "@/components/shared/Footer";
 import NavBar from "@/components/shared/NavBar";
 import ScrollToTopFab from "@/components/shared/ScrollToTopFab";
 
-import Analytics from "./analytics";
-import MuiProvider from "./mui-provider";
-import RootStyleRegistry from "./root-style-registry";
+import Analytics from "./Analytics";
+import MuiProvider from "./MuiProvider";
+import RootStyleRegistry from "./RootStyleRegistry";
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => (
   <html lang="en" className={rubik.className}>
