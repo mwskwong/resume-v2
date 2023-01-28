fbd8274ed8869478323c3f5c941289a8b4602e17
Matthew Kwong <matthewkwong2@gmail.com>
2023-01-29 00:56:30 +0800

    feat: Lazy load all sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
+import dynamic from "next/dynamic";
 import { FC } from "react";
 
-import About from "@/components/home/About";
-import Contact from "@/components/home/Contact";
-import Education from "@/components/home/Education";
-import Experience from "@/components/home/Experience";
-import Hero from "@/components/home/Hero";
 import WaveRoaring from "@/components/shared/dividers/WaveRoaring";
 import WaveRough from "@/components/shared/dividers/WaveRough";
 import WaveSmooth from "@/components/shared/dividers/WaveSmooth";
@@ -12,11 +8,11 @@ import WaveSmoothFlat from "@/components/shared/dividers/WaveSmoothFlat";
 
 import useSx from "./useSx";
 
-// const Home = dynamic(() => import("@/components/home/Home"));
-// const About = dynamic(() => import("@/components/home/About"));
-// const Experience = dynamic(() => import("@/components/home/Experience"));
-// const Education = dynamic(() => import("@/components/home/Education"));
-// const Contact = dynamic(() => import("@/components/home/Contact"));
+const Hero = dynamic(() => import("@/components/home/Hero"));
+const About = dynamic(() => import("@/components/home/About"));
+const Experience = dynamic(() => import("@/components/home/Experience"));
+const Education = dynamic(() => import("@/components/home/Education"));
+const Contact = dynamic(() => import("@/components/home/Contact"));
 
 const Home: FC = () => {
   const sx = useSx();
