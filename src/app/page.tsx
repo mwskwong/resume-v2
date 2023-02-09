065e513a784cfda36dd6f14563e37c1a468a4d76
Matthew Kwong <matthewkwong2@gmail.com>
2023-02-09 20:56:47 +0800

    chore: DIrect import Hero, About, Experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { FC, lazy, Suspense } from "react";
 
 import About from "@/components/home/About";
+import Experience from "@/components/home/Experience";
 import Hero from "@/components/home/Hero";
 import WaveRoaring from "@/components/shared/dividers/WaveRoaring";
 import WaveRough from "@/components/shared/dividers/WaveRough";
@@ -16,7 +17,7 @@ import { SectionId } from "@/types";
 
 // const Hero = lazy(() => import("@/components/home/Hero"));
 // const About = lazy(() => import("@/components/home/About"));
-const Experience = lazy(() => import("@/components/home/Experience"));
+// const Experience = lazy(() => import("@/components/home/Experience"));
 const Education = lazy(() => import("@/components/home/Education"));
 const Contact = lazy(() => import("@/components/home/Contact"));
 
@@ -45,9 +46,7 @@ const Home: FC = () => (
       nextSectionVariant={sectionVariants.experience}
     />
     <Section variant={sectionVariants.experience} id={EXPERIENCE.id} data-cy={EXPERIENCE.id}>
-      <Suspense>
-        <Experience />
-      </Suspense>
+      <Experience />
     </Section>
     <WaveSmoothFlat
       previousSectionVariant={sectionVariants.experience}
