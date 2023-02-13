0876a2e4e56cf71865a14d1250d8d305da25f3e3
Matthew Kwong <matthewkwong2@gmail.com>
2023-02-13 21:17:10 +0800

    refactor: Minor reformating

--- src/brandingTheme/rubik.ts
+++ src/brandingTheme/rubik.ts
@@ -1,5 +1,8 @@
 import { Rubik } from "@next/font/google";
 
-const rubik = Rubik({ subsets: ["latin"], display: "swap" });
+const rubik = Rubik({
+  subsets: ["latin"],
+  display: "swap"
+});
 
 export default rubik;
