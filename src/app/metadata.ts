d8a8acff0bec221198325571473839d5ed600398
Matthew Kwong <matthew.kwong@tecpal.com>
2023-02-17 16:55:08 +0800

    chore: Package update

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,81 +1,152 @@
 import { Metadata } from "next";
 
+
+
 import jobTitles from "@/constants/jobTitles";
+
 import { firstName, lastName } from "@/constants/name";
+
 import selfIntroduction from "@/constants/selfIntroduction";
 
+
+
 const siteUrl = process.env.NEXT_PUBLIC_URL;
+
 const fullName = `${firstName} ${lastName}`;
+
 const title: Metadata["title"] = {
+
   default: `${fullName} - ${jobTitles.join(" & ")}`,
+
   template: `%s | ${fullName}`
+
 };
+
 const description = selfIntroduction;
+
 const ogImage = `${siteUrl}/api/og`;
 
+
+
 const metadata: Metadata = {
+
   title,
+
   description,
+
   authors: { name: fullName },
+
   themeColor: "#ffffff",
+
   openGraph: {
+
     title,
+
     description,
+
     type: "website",
+
     url: siteUrl,
+
     images: ogImage
+
   },
+
   robots: {
+
     index: true,
+
     follow: true
+
   },
+
   icons: {
+
     icon: [
+
       {
+
         url: "/favicon.svg",
+
         type: "image/svg+xml"
+
       },
+
       {
+
         url: "/favicon-32x32.png",
+
         type: "image/png",
+
         sizes: "32x32"
+
       },
+
       {
+
         url: "/favicon-16x16.png",
+
         type: "image/png",
+
         sizes: "16x16"
+
       },
+
       {
+
         url: "/favicon-32x32.dark.png",
+
         type: "image/png",
+
         sizes: "32x32",
+
         // @ts-expect-error media is not a valid key
+
         media: "(prefers-color-scheme: dark)"
+
       },
+
       {
+
         url: "/favicon-16x16.dark.png",
+
         type: "image/png",
+
         sizes: "16x16",
+
         media: "(prefers-color-scheme: dark)"
+
       }
+
     ],
+
     apple: {
+
       url: "/apple-touch-icon.png",
+
       sizes: "180x180"
-    },
-    // FIXME: need proper manifest option
-    other: {
-      rel: "manifest",
-      url: "/api/manifest"
+
     }
+
   },
+
   twitter: {
+
     card: "summary_large_image",
+
     title,
+
     description,
+
     images: ogImage
+
   },
+
   manifest: "/api/manifest"
+
 };
 
+
+
 export default metadata;
+
