a51adf0de879d813166c694b2fcd352a7f078465
Matthew Kwong <matthew.kwong@tecpal.com>
2023-03-01 10:41:35 +0800

    refactor: Move emotion registry and analytics to `app/`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import rubik from "@/brandingTheme/rubik";
 import Footer from "@/components/shared/Footer";
 import NavBar from "@/components/shared/NavBar";
 import ScrollToTopFab from "@/components/shared/ScrollToTopFab";
-import Analytics from "@/utils/Analytics";
-import EmotionRegistry from "@/utils/EmotionRegistry";
+
+import Analytics from "./analytics";
+import EmotionRegistry from "./emotion-registry";
 
 const RootLayout: FC<Required<PropsWithChildren>> = ({ children }) => (
   <html lang="en" className={rubik.className}>
