6848179a02f266e9ac7d58e124078017016c7796
Matthew Kwong <matthew.kwong@tecpal.com>
2023-03-02 13:59:32 +0800

    chore: Minor rearranging config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,12 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:@typescript-eslint/strict",
-    "next/core-web-vitals",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "next/core-web-vitals",    
     "plugin:chai-friendly/recommended"
   ],
   "parser": "@typescript-eslint/parser",
