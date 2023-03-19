f3f73ef84a3857671d4e82b689dd240a2228ce2d
Matthew Kwong <matthewkwong2@gmail.com>
2023-03-19 08:59:09 +0800

    fix: `ownerState` get passed to dom when using framer motion

--- src/components/home/Education/Courses.tsx
+++ src/components/home/Education/Courses.tsx
@@ -18,6 +18,8 @@ import useSx from "./useCoursesSx";
 const loadFeatures = () =>
   import("./framerMotionFeatures").then((res) => res.default);
 
+const MotionGrid = m(Grid);
+
 const Courses: FC = () => {
   const sx = useSx();
   const [query, setQuery] = useState("");
@@ -60,13 +62,13 @@ const Courses: FC = () => {
               const certificateUrl = getCertificatePathById(id);
 
               return (
-                <Grid key={name} component={m.div} xs={12} md={6} layout>
+                <MotionGrid key={name} xs={12} md={6} layout>
                   <CertificateCard
                     name={name}
                     organization={institution}
                     certificateUrl={certificateUrl}
                   />
-                </Grid>
+                </MotionGrid>
               );
             })}
           </Grid>
