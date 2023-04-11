e3123d2627c5977c9509a92c3cd2eac15316b7a9
Matthew Kwong <matthewkwong2@gmail.com>
2023-04-11 14:03:40 +0800

    refactor: Use MUI sx props on pdf doc

--- src/components/shared/timeline/pdf-thumbnail.tsx
+++ src/components/shared/timeline/pdf-thumbnail.tsx
@@ -1,10 +1,32 @@
-import { Document, DocumentProps, Page } from "react-pdf";
+import { SxProps, Theme, styled } from "@mui/material";
+import { DocumentProps, Page, Document as PdfDocument } from "react-pdf";
 
-import styles from "./pdf-thumbnail.module.css";
+import cx from "@/utils/cx";
 
-export default function PdfThumbnail(props: DocumentProps) {
+interface Props extends DocumentProps {
+  sx?: SxProps<Theme>;
+}
+
+const Document = styled(PdfDocument)``;
+
+export default function PdfThumbnail({ sx, ...props }: Props) {
   return (
-    <Document loading="" error="" className={styles.document} {...props}>
+    <Document
+      loading=""
+      error=""
+      sx={cx(
+        {
+          borderRadius: 1,
+          mr: 2,
+          width: 100,
+          height: 56,
+          overflow: "hidden",
+          flexShrink: 0,
+        },
+        sx
+      )}
+      {...props}
+    >
       <Page
         pageNumber={1}
         width={100}
