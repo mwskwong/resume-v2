8188cfa76e538b80c755e9b81005a8c55c031699
Matthew Kwong <matthewkwong2@gmail.com>
2023-04-13 10:46:13 +0800

    feat: Lazy load PDF thumbnail

--- src/components/shared/timeline/timeline-item-content.tsx
+++ src/components/shared/timeline/timeline-item-content.tsx
@@ -8,9 +8,11 @@ import {
   Stack,
   StackProps,
 } from "@mui/material";
+import { Suspense, lazy } from "react";
 
-import PdfThumbnail from "./pdf-thumbnail";
 import { TimelineItemData } from "./types";
+
+const PdfThumbnail = lazy(() => import("./pdf-thumbnail"));
 
 export default function TimelineItemContent({
   contents = [],
@@ -47,7 +49,9 @@ export default function TimelineItemContent({
                 href={url}
                 target="_blank"
               >
-                <PdfThumbnail file={url} />
+                <Suspense>
+                  <PdfThumbnail file={url} />
+                </Suspense>
                 <ListItemText
                   primary={title}
                   primaryTypographyProps={{
