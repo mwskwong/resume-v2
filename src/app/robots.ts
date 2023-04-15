4030bddd4bfe197414207fae85205f8bd1e35a74
Matthew Kwong <matthewkwong2@gmail.com>
2023-04-15 14:56:48 +0800

    feat: Use dynamic robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    host: process.env.NEXT_PUBLIC_URL,
+    sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
+  };
+}
