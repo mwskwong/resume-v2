c1bda7b25c7b277c697bc3f5dc19c1af83e7a501
Matthew Kwong <matthew.kwong@tecpal.com>
2023-05-25 16:06:19 +0800

    fix: no need post install script

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require("fs");
-const path = require("path");
-
-fs.copyFileSync(
-  path.join(__dirname, "./node_modules/pdfjs-dist/build/pdf.worker.js"),
-  path.join(__dirname, "./public/pdf.worker.js")
-);
