3494e4d5593ecb006c91172ac2e3de43c541e483
Matthew Kwong <matthewkwong2@gmail.com>
2023-06-08 10:38:51 +0800

    refactor: simplify the code

--- src/api/get-educations.ts
+++ src/api/get-educations.ts
@@ -21,13 +21,13 @@ const getEducations = async () => {
         `https:${item.fields.school.fields.logo.fields.file.url}`,
     },
     supportingDocuments: item.fields.supportingDocuments
-      ?.map((supportingDocument) =>
-        supportingDocument?.fields.title && supportingDocument.fields.file
-          ? {
-              title: supportingDocument.fields.title,
-              url: `https:${supportingDocument.fields.file.url}`,
-            }
-          : undefined
+      ?.map(
+        (supportingDocument) =>
+          supportingDocument?.fields.title &&
+          supportingDocument.fields.file && {
+            title: supportingDocument.fields.title,
+            url: `https:${supportingDocument.fields.file.url}`,
+          }
       )
       .filter((elem): elem is { title: string; url: string } => Boolean(elem)),
   }));
